223e49386c8ae34facad9b1ebaf54ebbecb05d62
ProgLab25 <mneryllene@gmail.com>
2025-12-01 22:35:35 +0000

    Nível Mestre

---
+++ Des_War_Estruturado01/Nível Mestre.c
@@ -0,0 +1,12 @@
+#include <stdio.h>  
+#include <stdlib.h> 
+#include <time.h>  
+#include <string.h> 
+
+// STRUCTS DO JOGO
+typedef struct{
+    char nome[30];
+    char cor[10];
+    int tropas;
+ }Territorio;
+
